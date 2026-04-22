836078f31a593610cdef0de8cf9c166721f46293
Jhostyn Andrade <e21105@ism.edu.ec>
2026-04-22 08:10:41 -0500

    Evaluacion par

--- Actividad 4 Strings.py
+++ Actividad 4 Strings.py
@@ -4,20 +4,45 @@ Castro Noam
 Informatica 
 Elian Rome"""
 print(stringVariaslineas)
+
 colegio="Ism"
 longitud=len(colegio)
 print(longitud)
 print(len(colegio))
 print(len("loco i"))
+
 nombre= "Jhostyn"
 apellido= "Andrade"
 nombrecompleto=nombre+" "+apellido
 print(nombrecompleto)
 print(f"mi nombre completo es: {nombrecompleto}")
 print("mi nombre es: "+nombrecompleto)
+
 print(nombrecompleto*4)
+
 print("Python\nChallenge\nXD")
+
 print("nombre\tsemana1\tsemana2\tsemana3")
 print("simbolo(\\)")
+
 #añadir f antes de las comillas
 print(f"Mi nombre es {nombre} y mi apellido es {apellido}")
+
+letraporletra="hola"
+a ,b ,c ,d =letraporletra
+print(b)
+
+language="Python"
+print(language[0])
+print(language[-3:])
+print(language[0:2]) #el ultimo valor no le cuenta
+print(language[::-1]) #para invertir la palabra
+print(language[0:6:5])#posicion inicial, posicion final y cuanto se va a saltar
+
+cambiomayuscula="treinta dias"
+print(cambiomayuscula.capitalize()) #En mayuscula la primera palabra
+print(cambiomayuscula.count("a")) #cuantas veces se repite
+print(cambiomayuscula.count("treinta"))
+
+
+
