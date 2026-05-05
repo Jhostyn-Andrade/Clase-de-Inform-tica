7a07e57eab87c4b7c135aa824a763c69995f0d0d
Jhostyn Andrade <e21105@ism.edu.ec>
2026-05-05 10:55:45 -0500

    while cn imput

--- Ciclo while.py
+++ Ciclo while.py
@@ -1,7 +1,12 @@
-variabledecontrol= 8
+"""variabledecontrol= 8
 while variabledecontrol<5:
     print(f"Esta es la vuelta {variabledecontrol}")
     variabledecontrol += 1
 else:
     print("No se cumplió el ciclo")
-print("Fin del programa") 
+print("Fin del programa") """
+
+clave=""
+while clave != "python123":
+    clave=input("Ingrese la clave:")
+print("Clave correcta. Acceso concedido.")
