fc1d75282602bbb13786250900c9a25be912f112
Jhostyn Andrade <e21105@ism.edu.ec>
2026-05-05 10:46:55 -0500

    Ciclo while

---
+++ Ciclo while.py
@@ -0,0 +1,7 @@
+variabledecontrol= 8
+while variabledecontrol<5:
+    print(f"Esta es la vuelta {variabledecontrol}")
+    variabledecontrol += 1
+else:
+    print("No se cumplió el ciclo")
+print("Fin del programa") 
