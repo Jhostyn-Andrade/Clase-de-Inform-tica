391ea1c349ad8aeb3bf7b662336cb57c9434642b
Jhostyn Andrade <e21105@ism.edu.ec>
2026-05-05 11:04:37 -0500

    actividad de if y else

--- Ciclo while.py
+++ Ciclo while.py
@@ -6,7 +6,22 @@ else:
     print("No se cumplió el ciclo")
 print("Fin del programa") """
 
-clave=""
+"""clave=""
 while clave != "python123":
     clave=input("Ingrese la clave:")
-print("Clave correcta. Acceso concedido.")
+print("Clave correcta. Acceso concedido.") """
+
+opcion=""
+while opcion!="c":
+    print("a=Saludar")
+    print("b=mostrar mensaje")
+    print("c=salir")
+    opcion=input("seleccione una opción: ")
+    if opcion=="a":
+        print("Hola, BIENVENIDO")
+    elif opcion=="b":
+        print("Estamos aprendiendo ciclo while")
+    elif opcion=="c":
+        print("programa terminado")
+    else:
+        print("funcion invalida")
