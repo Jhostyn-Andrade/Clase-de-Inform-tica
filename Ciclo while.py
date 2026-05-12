be4aa8915310989386193b847fc6910edec9be53
Jhostyn Andrade <e21105@ism.edu.ec>
2026-05-12 12:13:15 -0500

    Inicio de ciclo FOR IN

--- Ciclo while.py
+++ Ciclo while.py
@@ -11,7 +11,7 @@ while clave != "python123":
     clave=input("Ingrese la clave:")
 print("Clave correcta. Acceso concedido.") """
 
-opcion=""
+"""opcion=""
 while opcion!="c":
     print("a=Saludar")
     print("b=mostrar mensaje")
@@ -24,4 +24,16 @@ while opcion!="c":
     elif opcion=="c":
         print("programa terminado")
     else:
-        print("funcion invalida")
+        print("funcion invalida")"""  
+
+#CICLO FOR
+numers=[0,1,2,3,4,5]
+for iterador in numers:
+    print(f"Su itererador es {iterador}")
+
+notas=[8,7,9,10]
+suma=0
+for nota in notas:
+    suma=suma+nota
+promedio=suma/len(notas)
+print(promedio)
